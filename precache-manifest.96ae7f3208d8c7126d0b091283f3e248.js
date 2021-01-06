2e09c64926cc03a866253244cc443d256796a036
avigael <code@gaels.us>
2021-01-06 01:13:32 -0600

    Updates

---
+++ precache-manifest.96ae7f3208d8c7126d0b091283f3e248.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d48ad59047dffebc2df0b14ad669ec7",
+    "url": "/education-dashboard/index.html"
+  },
+  {
+    "revision": "a06c2451ad0b3197abd6",
+    "url": "/education-dashboard/static/css/main.978039e2.chunk.css"
+  },
+  {
+    "revision": "6dae9a70fcd0c26e1a8c",
+    "url": "/education-dashboard/static/js/2.204f3370.chunk.js"
+  },
+  {
+    "revision": "a06c2451ad0b3197abd6",
+    "url": "/education-dashboard/static/js/main.68378537.chunk.js"
+  },
+  {
+    "revision": "d67672d395d7e5251227",
+    "url": "/education-dashboard/static/js/runtime~main.1b6f9841.js"
+  }
+]);
